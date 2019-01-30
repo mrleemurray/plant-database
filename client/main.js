709b4de51435a86033e293ce3a1f46f01585ca74
mrleemurray <lm@spyscape.com>
2019-01-30 16:42:44 -0600

    further secured subscribe & publish methods

--- client/main.js
+++ client/main.js
@@ -6,5 +6,6 @@ import App from '../imports/ui/App.js';
 import '../imports/startup/accounts-config.js';
  
 Meteor.startup(() => {
+  // navigator.serviceWorker.register('/sw.js') .then() .catch(error => console.log('ServiceWorker registration failed: ', err));
   render(<App />, document.getElementById('render-target'));
 });
--- imports/ui/Task.js
+++ imports/ui/Task.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
 
 import { Tasks } from '../api/tasks.js';
  
@@ -14,11 +15,18 @@ export default class Task extends Component {
       deleteThisTask() {
         Meteor.call('tasks.remove', this.props.task._id);
       }
+
+      togglePrivate() {
+        Meteor.call('tasks.setPrivate', this.props.task._id, ! this.props.task.private);
+      }
     render() {
         // Give tasks a different className when they are checked off,
         // so that we can style them nicely in CSS
-        const taskClassName = this.props.task.checked ? 'checked' : '';
 
+        const taskClassName = classnames({
+            checked: this.props.task.checked,
+            private: this.props.task.private,
+        });
         return (
             <li className={taskClassName}>
                 <button className="delete" onClick={this.deleteThisTask.bind(this)}>
@@ -31,6 +39,12 @@ export default class Task extends Component {
                 checked={!!this.props.task.checked}
                 onClick={this.toggleChecked.bind(this)}
                 />
+
+                { this.props.showPrivateButton ? (
+                        <button className="toggle-private" onClick={this.togglePrivate.bind(this)}>
+                            { this.props.task.private ? 'Private' : 'Public' }
+                        </button>
+                ) : ''}
         
                 <span className="text">
                     <strong>{this.props.task.username}</strong>: {this.props.task.text}
